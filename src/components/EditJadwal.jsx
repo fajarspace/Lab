34e64f827c787d1310fc26a63ccd81724f8b4007
fajar.agngn <fajar.agngn@gmail.com>
2023-03-20 23:57:19 +0700

    'deploy'

--- src/components/EditJadwal.jsx
+++ src/components/EditJadwal.jsx
@@ -93,7 +93,7 @@ const EditJadwal = () => {
                 <option value="09:30 - 11:30">09:30 - 11:30</option>
                 <option value="13:00 - 15:00">13:00 - 15:00</option>
                 <option value="15:30 - 17:30">15:30 - 17:30</option>
-                <option value="18:30 - 20:00">18:30 - 20:00</option>
+                <option value="18:30 - 20:00">18:00 - 20:00</option>
                 <option value="20:00 - 22:00">20:00 - 22:00</option>
 
               </select>
